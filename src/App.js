24ea9fe1a6597dec054823bce6ed000912d7cb7a
Nick Airdrie <nick.airdrie@gmail.com>
2020-01-14 12:36:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './res/logo-color.svg';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Nick Airdrie<div className="glitch1">Nick Airdrie</div><div className="glitch2">Nick Airdrie</div></h1>
+        <h2>Computer Engineering Student, University of Guelph</h2>
+        <div className="links">
+								<span>
+									<a target="_blank" href="https://ca.linkedin.com/in/nairdrie"><FontAwesomeIcon icon={faLinkedin} /></a>
+									<a target="_blank" href="https://github.com/nairdrie"><FontAwesomeIcon icon={faGithubSquare} /></a>
+									<a target="_blank" href="https://www.instagram.com/nick.airdrie/"><FontAwesomeIcon icon={faInstagram} /></a>
+								</span>
+							</div>
+        <p className="bio">Hello world! Welcome to my portfolio. I am a fifth year Computer Engineering student studying at the University of Guelph. 
+        I designed and built this site from scratch with React. I am passionate about programming, solving problems, and learning new things.</p>
+        <div className="menu">
+								<span>
+									<button>INTRO<div className="menuglitch1">INTRO</div><div className="menuglitch2">INTRO</div></button>
+									<button>EXPERIENCE<div className="menuglitch1">EXPERIENCE</div><div className="menuglitch2">EXPERIENCE</div></button>
+									<button>PROJECTS<div className="menuglitch1">PROJECTS</div><div className="menuglitch2">PROJECTS</div></button>
+                  <button>RESUME<div className="menuglitch1">RESUME</div><div className="menuglitch2">RESUME</div></button>
+                  <button>CONTACT<div className="menuglitch1">CONTACT</div><div className="menuglitch2">CONTACT</div></button>
+								</span>
+							</div>
       </header>
     </div>
   );
