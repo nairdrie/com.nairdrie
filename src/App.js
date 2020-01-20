4035b9eebe849fa1ced03b192989c7b119b71ce7
Nick Airdrie <nick.airdrie@gmail.com>
2020-01-20 18:20:16 -0500

    found a fullpage addon that will work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import './App.scss';
 
 import { Pager } from "react-bootstrap";
@@ -13,9 +14,7 @@ import Projects from "./Pages/Projects";
 import Resume from "./Pages/Resume";
 import Contact from "./Pages/Contact";
 
-import { FullPage, Slide } from 'react-full-page'
-
-
+import ReactFullpage from '@fullpage/react-fullpage';
 
 class App extends React.Component {
   constructor(props) {
@@ -25,11 +24,7 @@ class App extends React.Component {
     this.fullPageRef = React.createRef();
   }
 
-
-
-  handlePageChange = number => {
-    this.setState({ currentPage: number, activeMenuItem:number }); // set currentPage number, to reset it from the previous selected.
-  };
+  
 
   getPagesNumbers = () => {
     const pageNumbers = [];
@@ -46,9 +41,7 @@ class App extends React.Component {
   };
 
   handleMenuClick = (clickedItemIndex) => {
-    this.setState({activeMenuItem:clickedItemIndex});
-    this.handlePageChange(clickedItemIndex);
-    this.fullPageRef.current.scrollToSlide(clickedItemIndex);
+    this.setState({ currentPage: clickedItemIndex, activeMenuItem:clickedItemIndex }); 
   }
 
   scrollHandler = (status) => {
@@ -62,8 +55,9 @@ class App extends React.Component {
     const pagesNumbers = this.getPagesNumbers();
 
     return <div className="App">
-        <PersistentMenu currentPage={this.state.currentPage} activeMenuItem={this.state.activeMenuItem} onMenuClick={this.handleMenuClick}/>
-        <PageDots currentPage={this.state.currentPage}  />
+        <PersistentMenu currentPage={this.state.currentPage} activeMenuItem={this.state.activeMenuItem} onMenuClick={(index) => {this.handleMenuClick(index);fullpageapi.moveTo(['intro', 'experience', 'projects', 'resume', 'contact'][index], 0)}}/>
+        {/*<PageDots currentPage={this.state.currentPage}  />*/}
+
         {/*<ReactPageScroller
           pageOnChange={this.handlePageChange}
           customPageNumber={this.state.currentPage}
@@ -74,7 +68,7 @@ class App extends React.Component {
           <Resume />
           <Contact />
         </ReactPageScroller>*/}
-        <FullPage beforeChange={this.scrollHandler} duration={1000} ref={this.fullPageRef}>
+        {/*<FullPage beforeChange={this.scrollHandler} duration={1000} ref={this.fullPageRef}>
           <Slide>
             <Intro activeMenuItem={this.state.activeMenuItem} onMenuClick={this.handleMenuClick}/>
           </Slide>
@@ -91,10 +85,49 @@ class App extends React.Component {
             <Contact />
           </Slide>
         </FullPage>
+      */}
         {/*<Pager className="pagination-additional-class" bsSize="large">
           {pagesNumbers}
         </Pager>
         */}
+         <ReactFullpage
+          debug /* Debug logging */
+
+
+
+          // fullpage options
+          //licenseKey={'YOUR_KEY_HERE'} // Get one from https://alvarotrigo.com/fullPage/pricing/
+          navigation
+          anchors={['intro', 'experience', 'projects', 'resume', 'contact']}
+          //sectionSelector={SECTION_SEL}
+          //onLeave={this.onLeave.bind(this)}
+          //sectionsColor={this.state.sectionsColor}
+
+          render={({ state, fullpageApi }) => (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <Intro activeMenuItem={this.state.activeMenuItem} onMenuClick={(index) => {this.handleMenuClick(index);fullpageApi.moveTo(['intro', 'experience', 'projects', 'resume', 'contact'][index], 0)}}/>
+              </div>
+              <div className="section">
+                <Experience />
+              </div>
+              <div className="section">
+                <Projects />
+              </div>
+              <div className="section">
+                <Resume />
+              </div>
+              <div className="section">
+                <Contact />
+              </div>
+              
+              
+              
+              
+              
+            </ReactFullpage.Wrapper>
+          )}
+        />
     </div>;
   }
 }
